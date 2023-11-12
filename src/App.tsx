41273819c9c759dbce345c621202c006f0aaec0b
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-11-12 20:54:58 +0900

    トッププロフィールページの雛形作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,54 @@
-export const App = () => {
-  return <h1>プロフィールサイト</h1>;
+import profileImage from "./images/profile.png";
+import styled from "styled-components";
+
+// プロフィール情報
+const profile = {
+  name: "Osamu Ishiyama",
+  gender: "Male",
+  birth: "2000/02/04",
+  address: "Tokyo/Japan",
+  job: "Software Engineer",
 };
+
+export const App = () => {
+  return (
+    <>
+      <SPageTitle>Profile</SPageTitle>
+      <SProfileContainer>
+        <SProfileImage src={profileImage} />
+        <ul>
+          {Object.entries(profile).map(([k, v]) => (
+            <SProfileList key={k}>
+              {k.charAt(0).toUpperCase() + k.slice(1)}: {v}
+            </SProfileList>
+          ))}
+        </ul>
+      </SProfileContainer>
+    </>
+  );
+};
+
+const SPageTitle = styled.h1`
+  text-align: center;
+  font-family: sans-serif;
+  margin: 20px 0 30px 0;
+`;
+
+const SProfileContainer = styled.div`
+  margin: 0 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+`;
+
+// todo: レスポンシブ対応にする必要あり
+const SProfileImage = styled.img`
+  height: 300px;
+  width: 300px;
+`;
+
+const SProfileList = styled.li`
+  margin: 5px 0;
+  font-family: sans-serif;
+`;
