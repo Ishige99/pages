8f772ef1caada5c66da59e5e31912777fbc39749
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-11-15 22:42:33 +0900

    todoの記載。GitHubPagesデプロイ後に直リンクを踏むと404になるので別途対応必要

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { Profile } from "./components/Profile";
 import { Link } from "./components/Link";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// todo: 直リンク入力だと未レンダリングにより404になるので、リダイレクトなりの対応が必要
+// https://stackoverflow.com/questions/46056414/getting-404-for-links-with-create-react-app-deployed-to-github-pages
+// https://qiita.com/takuya0206/items/f284b5e68f48f1ebefae
+
 export const App = () => {
   return (
     <BrowserRouter>
