0bd984ae280fb048b282a9edcc9bf5e7b58f2874
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-11-16 23:02:53 +0900

    ヘッダーにスタイルの適用

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,29 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import headerLogo from "../images/header_logo.png";
 
 export const Header = () => {
   return (
-    <>
-      <img src={headerLogo} />
-      <Link to={"/pages/links"}>Links</Link>
-    </>
+    <SHeader>
+      <SHeaderLogo src={headerLogo} />
+      <div>
+        <SLink to={"/pages/links"}>Links</SLink>
+      </div>
+    </SHeader>
   );
 };
+
+const SHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid black;
+`;
+const SHeaderLogo = styled.img`
+  width: 70px;
+  height: 70px;
+  margin: 0 0 0 30px;
+`;
+const SLink = styled(Link)`
+  margin-right: 100px;
+`;
