72caf4ff811ae4d33fde83d6f42d71495511c70f
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-11-18 23:12:46 +0900

    profileページの修正

--- src/components/ProfileInfo.tsx
+++ src/components/ProfileInfo.tsx
@@ -1,10 +1,8 @@
 import profileImage from "../images/profile.png";
 import styled from "styled-components";
 
-// プロフィール情報
 const profile = {
   name: "Osamu Ishiyama",
-  gender: "Male",
   birth: "2000/02/04",
   address: "Tokyo/Japan",
   job: "Software Engineer",
@@ -14,32 +12,32 @@ export const ProfileInfo = () => {
   return (
     <SProfileContainer>
       <SProfileImage src={profileImage} />
-      <ul>
-        {Object.entries(profile).map(([k, v]) => (
-          <SProfileList key={k}>
-            {k.charAt(0).toUpperCase() + k.slice(1)}: {v}
-          </SProfileList>
-        ))}
-      </ul>
+      <div>
+        <SProfileList>Name: {profile.name}</SProfileList>
+        <SProfileList>Birth: {profile.birth}</SProfileList>
+        <SProfileList>Address: {profile.address}</SProfileList>
+        <SProfileList>Job: {profile.job}</SProfileList>
+      </div>
     </SProfileContainer>
   );
 };
 
 const SProfileContainer = styled.div`
-  margin: 0 100px;
+  width: 80%;
+  margin: 0 auto;
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 30px;
+  flex-wrap: wrap;
+  gap: 50px;
 `;
-
 // todo: レスポンシブ対応にする必要あり
 const SProfileImage = styled.img`
-  height: 300px;
-  width: 300px;
+  height: 250px;
+  width: 250px;
 `;
-
-const SProfileList = styled.li`
-  margin: 5px 0;
+const SProfileLists = styled.div`
+`
+const SProfileList = styled.p`
   font-family: Arial;
 `;
