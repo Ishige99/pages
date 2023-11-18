87c9d763d6520c36b996f3256219e5cce3cd1bf0
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-11-18 19:44:05 +0900

    全体の横幅調整

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Portfolio } from "./components/Portfolio";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
 // todo: 直リンク入力だと未レンダリングにより404になるので、リダイレクトなりの対応が必要
 // https://stackoverflow.com/questions/46056414/getting-404-for-links-with-create-react-app-deployed-to-github-pages
@@ -11,14 +12,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/pages" element={<Profile />} />
-        <Route path="/pages/link" element={<Link />} />
-        <Route path="/pages/portfolio" element={<Portfolio />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <SAppBody>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/pages" element={<Profile />} />
+          <Route path="/pages/link" element={<Link />} />
+          <Route path="/pages/portfolio" element={<Portfolio />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </SAppBody>
   );
 };
+
+const SAppBody = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`
